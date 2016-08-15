559aced1f4d649967d3aa2e8d1e167f2064657a9
Alexandr Savinov <savinov@conceptoriented.org>
2016-08-15 19:45:57 +0200

    change position of data panel and data buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }      from '@angular/core';
-//import { CommonModule }       from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }        from '@angular/forms';
+//import { CommonModule }       from '@angular/common';
 
 import { HttpModule } from '@angular/http';
 
